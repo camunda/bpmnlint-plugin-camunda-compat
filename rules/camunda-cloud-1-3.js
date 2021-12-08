ff44f7d392d4a432f0a31fb019a6a15038ed41d0
Philipp <philippfromme@outlook.com>
2021-12-08 15:56:10 +0100

    feat: return early if engine profile not matching

--- rules/camunda-cloud-1-3.js
+++ rules/camunda-cloud-1-3.js
@@ -2,4 +2,4 @@ const { createRule } = require('./utils/rule');
 
 const checks = require('./camunda-cloud-1-3-checks');
 
-module.exports = createRule('1.3', checks);
+module.exports = createRule('Camunda Cloud', '1.3', checks);
