41731c99f59e729009e256c692f2a64242d0b890
Philipp <philippfromme@outlook.com>
2022-07-07 14:56:42 +0200

    fix: add name (again)
    Adding the name of the node was accidentally removed in https://github.com/camunda/bpmnlint-plugin-camunda-compat/pull/29.

--- rules/utils/reporter.js
+++ rules/utils/reporter.js
@@ -1,3 +1,5 @@
+const { is } = require('bpmnlint-utils');
+
 const { isArray } = require('min-dash');
 
 module.exports.reportErrors = function(node, reporter, errors) {
@@ -6,6 +8,31 @@ module.exports.reportErrors = function(node, reporter, errors) {
   }
 
   errors.forEach(({ message, ...options }) => {
+    const name = getName(node);
+
+    if (name) {
+      options = {
+        ...options,
+        name
+      };
+    }
+
     reporter.report(node.get('id'), message, options);
   });
 };
+
+function getName(node) {
+  if (is(node, 'bpmn:TextAnnotation')) {
+    return node.get('text');
+  }
+
+  if (is(node, 'bpmn:Group')) {
+    const categoryValueRef = node.get('categoryValueRef');
+
+    return categoryValueRef && categoryValueRef.get('value');
+  }
+
+  return node.get('name');
+}
+
+module.exports.getName = getName;
---
+++ test/utils/reporter.spec.js
@@ -0,0 +1,157 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
+const sinon = require('sinon');
+
+const sinonChai = require('sinon-chai');
+
+chai.should();
+chai.use(sinonChai);
+
+const {
+  getName,
+  reportErrors
+} = require('../../rules/utils/reporter');
+
+const { createElement } = require('../helper');
+
+describe('utils/reporter', function() {
+
+  describe('#getName', function() {
+
+    it('group', function() {
+
+      // given
+      const group = createElement('bpmn:Group', {
+        categoryValueRef: createElement('bpmn:CategoryValue', {
+          value: 'Foo'
+        })
+      });
+
+      // when
+      const name = getName(group);
+
+      // then
+      expect(name).to.equal('Foo');
+    });
+
+
+    it('flow element', function() {
+
+      // given
+      const task = createElement('bpmn:Task', {
+        name: 'Foo'
+      });
+
+      // when
+      const name = getName(task);
+
+      // then
+      expect(name).to.equal('Foo');
+    });
+
+
+    it('text annotation', function() {
+
+      // given
+      const textAnnotation = createElement('bpmn:TextAnnotation', {
+        text: 'Foo'
+      });
+
+      // when
+      const name = getName(textAnnotation);
+
+      // then
+      expect(name).to.equal('Foo');
+    });
+
+  });
+
+  describe('#reportErrors', function() {
+
+    it('should report errors', function() {
+
+      // given
+      const node = createElement('bpmn:Task', {
+        id: 'Task_1'
+      });
+
+      const reportSpy = sinon.spy();
+
+      const reporter = {
+        report: reportSpy
+      };
+
+      const errors = [
+        {
+          message: 'foo',
+          foo: 'bar'
+        },
+        {
+          message: 'bar'
+        }
+      ];
+
+      // when
+      reportErrors(node, reporter, errors);
+
+      // then
+      expect(reportSpy).to.have.been.calledTwice;
+
+      expect(reportSpy.firstCall.args).to.eql([
+        'Task_1',
+        'foo',
+        {
+          foo: 'bar'
+        }
+      ]);
+
+      expect(reportSpy.secondCall.args).to.eql([
+        'Task_1',
+        'bar',
+        {}
+      ]);
+    });
+
+
+    it('should add name', function() {
+
+      // given
+      const node = createElement('bpmn:Task', {
+        id: 'Task_1',
+        name: 'Foo'
+      });
+
+      const reportSpy = sinon.spy();
+
+      const reporter = {
+        report: reportSpy
+      };
+
+      const errors = [
+        {
+          message: 'foo',
+          foo: 'bar'
+        }
+      ];
+
+      // when
+      reportErrors(node, reporter, errors);
+
+      // then
+      expect(reportSpy).to.have.been.calledOnce;
+
+      expect(reportSpy.firstCall.args).to.eql([
+        'Task_1',
+        'foo',
+        {
+          foo: 'bar',
+          name: 'Foo'
+        }
+      ]);
+    });
+
+  });
+
+});
