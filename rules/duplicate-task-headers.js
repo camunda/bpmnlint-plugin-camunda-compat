7e70764670fe5bb17e34dce14e78556732139a5a
Philipp <philippfromme@outlook.com>
2022-08-18 10:45:55 +0200

    feat(rules): add `duplicate-task-headers` rule
    Related to https://github.com/camunda/linting/issues/4
    Co-authored-by: Niklas Kiefer <niklas.kiefer@camunda.com>

---
+++ rules/duplicate-task-headers.js
@@ -0,0 +1,57 @@
+const {
+  is,
+  isAny
+} = require('bpmnlint-utils');
+
+const {
+  findExtensionElement,
+  getMessageEventDefinition,
+  hasDuplicatedPropertyValues
+} = require('./utils/element');
+
+const { reportErrors } = require('./utils/reporter');
+
+module.exports = function() {
+  function check(node, reporter) {
+    if (!is(node, 'bpmn:UserTask') && !isZeebeServiceTask(node)) {
+      return;
+    }
+
+    const taskHeaders = findExtensionElement(node, 'zeebe:TaskHeaders');
+
+    if (!taskHeaders) {
+      return;
+    }
+
+    const errors = hasDuplicatedPropertyValues(taskHeaders, 'values', 'key', node);
+
+    if (errors && errors.length) {
+      reportErrors(node, reporter, errors);
+    }
+  }
+
+  return {
+    check
+  };
+};
+
+// helpers //////////
+
+function isZeebeServiceTask(element) {
+  if (is(element, 'zeebe:ZeebeServiceTask')) {
+    return true;
+  }
+
+  if (isAny(element, [
+    'bpmn:EndEvent',
+    'bpmn:IntermediateThrowEvent'
+  ])) {
+    return getMessageEventDefinition(element);
+  }
+
+  if (is(element, 'bpmn:BusinessRuleTask')) {
+    return findExtensionElement(element, 'zeebe:TaskDefinition');
+  }
+
+  return false;
+}
