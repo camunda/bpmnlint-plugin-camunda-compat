8cededfb75ef74d62be23e2a7ec66e93adc95262
Philipp <philippfromme@outlook.com>
2022-09-08 14:06:24 +0200

    chore: clean up config
    * avoid copy paste errors :tm:

--- rules/element-type/config.js
+++ rules/element-type/config.js
@@ -1,186 +1,103 @@
+const camundaCloud10Elements = [
+  'bpmn:Association',
+  'bpmn:BoundaryEvent',
+  'bpmn:CallActivity',
+  'bpmn:Collaboration',
+  'bpmn:Definitions',
+  'bpmn:EndEvent',
+  'bpmn:EventBasedGateway',
+  'bpmn:ExclusiveGateway',
+  'bpmn:Group',
+  'bpmn:IntermediateCatchEvent',
+  'bpmn:MessageFlow',
+  'bpmn:ParallelGateway',
+  'bpmn:Participant',
+  'bpmn:Process',
+  'bpmn:ReceiveTask',
+  'bpmn:SequenceFlow',
+  'bpmn:ServiceTask',
+  'bpmn:StartEvent',
+  'bpmn:SubProcess',
+  'bpmn:TextAnnotation',
+  'bpmn:UserTask'
+];
+
+const camundaCloud11Elements = [
+  ...camundaCloud10Elements,
+  'bpmn:BusinessRuleTask',
+  'bpmn:IntermediateThrowEvent',
+  'bpmn:ManualTask',
+  'bpmn:ScriptTask',
+  'bpmn:SendTask'
+];
+
+const camundaCloud12Elements = [
+  ...camundaCloud11Elements
+];
+
+const camundaCloud10ElementsNoEventDefinitionRequired = [
+  'bpmn:EndEvent',
+  'bpmn:StartEvent'
+];
+
+const camundaCloud11ElementsNoEventDefinitionRequired = [
+  ...camundaCloud10ElementsNoEventDefinitionRequired,
+  'bpmn:IntermediateThrowEvent'
+];
+
+const camundaCloud12ElementsNoEventDefinitionRequired = [
+  ...camundaCloud11ElementsNoEventDefinitionRequired
+];
+
+const camundaCloud10EventDefinitions = {
+  'bpmn:BoundaryEvent': [
+    'bpmn:ErrorEventDefinition',
+    'bpmn:MessageEventDefinition',
+    'bpmn:TimerEventDefinition'
+  ],
+  'bpmn:EndEvent': [
+    'bpmn:ErrorEventDefinition'
+  ],
+  'bpmn:IntermediateCatchEvent': [
+    'bpmn:MessageEventDefinition',
+    'bpmn:TimerEventDefinition'
+  ],
+  'bpmn:StartEvent': [
+    'bpmn:ErrorEventDefinition',
+    'bpmn:MessageEventDefinition',
+    'bpmn:TimerEventDefinition'
+  ]
+};
+
+const camundaCloud11EventDefinitions = {
+  ...camundaCloud10EventDefinitions
+};
+
+const camundaCloud12EventDefinitions = {
+  ...camundaCloud11EventDefinitions,
+  'bpmn:EndEvent': [
+    ...camundaCloud11EventDefinitions['bpmn:EndEvent'],
+    'bpmn:MessageEventDefinition'
+  ],
+  'bpmn:IntermediateThrowEvent': [
+    'bpmn:MessageEventDefinition'
+  ]
+};
+
 module.exports = {
   camundaCloud10: {
-    elements: [
-      'bpmn:Association',
-      'bpmn:BoundaryEvent',
-      'bpmn:CallActivity',
-      'bpmn:Collaboration',
-      'bpmn:Definitions',
-      'bpmn:EndEvent',
-      'bpmn:EventBasedGateway',
-      'bpmn:ExclusiveGateway',
-      'bpmn:Group',
-      'bpmn:IntermediateCatchEvent',
-      'bpmn:MessageFlow',
-      'bpmn:ParallelGateway',
-      'bpmn:Participant',
-      'bpmn:Process',
-      'bpmn:ReceiveTask',
-      'bpmn:SequenceFlow',
-      'bpmn:ServiceTask',
-      'bpmn:StartEvent',
-      'bpmn:SubProcess',
-      'bpmn:TextAnnotation',
-      'bpmn:UserTask'
-    ],
-    elementsNoEventDefinitionRequired: [
-      'bpmn:EndEvent',
-      'bpmn:StartEvent'
-    ],
-    eventDefinitions: {
-      'bpmn:BoundaryEvent': [
-        'bpmn:ErrorEventDefinition',
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ],
-      'bpmn:EndEvent': [
-        'bpmn:ErrorEventDefinition'
-      ],
-      'bpmn:IntermediateCatchEvent': [
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ],
-      'bpmn:StartEvent': [
-        'bpmn:ErrorEventDefinition',
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ]
-    }
+    elements: camundaCloud10Elements,
+    elementsNoEventDefinitionRequired: camundaCloud10ElementsNoEventDefinitionRequired,
+    eventDefinitions: camundaCloud10EventDefinitions
   },
   camundaCloud11: {
-    elements: [
-
-      // 1.0
-      'bpmn:Association',
-      'bpmn:BoundaryEvent',
-      'bpmn:CallActivity',
-      'bpmn:Collaboration',
-      'bpmn:Definitions',
-      'bpmn:EndEvent',
-      'bpmn:EventBasedGateway',
-      'bpmn:ExclusiveGateway',
-      'bpmn:Group',
-      'bpmn:IntermediateCatchEvent',
-      'bpmn:MessageFlow',
-      'bpmn:ParallelGateway',
-      'bpmn:Participant',
-      'bpmn:Process',
-      'bpmn:ReceiveTask',
-      'bpmn:SequenceFlow',
-      'bpmn:ServiceTask',
-      'bpmn:StartEvent',
-      'bpmn:SubProcess',
-      'bpmn:TextAnnotation',
-      'bpmn:UserTask',
-
-      // 1.1
-      'bpmn:BusinessRuleTask',
-      'bpmn:IntermediateThrowEvent',
-      'bpmn:ManualTask',
-      'bpmn:ScriptTask',
-      'bpmn:SendTask'
-    ],
-    elementsNoEventDefinitionRequired: [
-
-      // 1.0
-      'bpmn:EndEvent',
-      'bpmn:StartEvent',
-
-      // 1.1
-      'bpmn:IntermediateThrowEvent'
-    ],
-    eventDefinitions: {
-      'bpmn:BoundaryEvent': [
-        'bpmn:ErrorEventDefinition',
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ],
-      'bpmn:EndEvent': [
-        'bpmn:ErrorEventDefinition'
-      ],
-      'bpmn:IntermediateCatchEvent': [
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ],
-      'bpmn:StartEvent': [
-        'bpmn:ErrorEventDefinition',
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ]
-    }
+    elements: camundaCloud11Elements,
+    elementsNoEventDefinitionRequired: camundaCloud11ElementsNoEventDefinitionRequired,
+    eventDefinitions: camundaCloud11EventDefinitions
   },
   camundaCloud12: {
-    elements: [
-
-      // 1.0
-      'bpmn:Association',
-      'bpmn:BoundaryEvent',
-      'bpmn:CallActivity',
-      'bpmn:Collaboration',
-      'bpmn:Definitions',
-      'bpmn:EndEvent',
-      'bpmn:EventBasedGateway',
-      'bpmn:ExclusiveGateway',
-      'bpmn:Group',
-      'bpmn:IntermediateCatchEvent',
-      'bpmn:MessageFlow',
-      'bpmn:ParallelGateway',
-      'bpmn:Participant',
-      'bpmn:Process',
-      'bpmn:ReceiveTask',
-      'bpmn:SequenceFlow',
-      'bpmn:ServiceTask',
-      'bpmn:StartEvent',
-      'bpmn:SubProcess',
-      'bpmn:TextAnnotation',
-      'bpmn:UserTask',
-
-      // 1.1
-      'bpmn:BusinessRuleTask',
-      'bpmn:IntermediateThrowEvent',
-      'bpmn:ManualTask',
-      'bpmn:ScriptTask',
-      'bpmn:SendTask'
-    ],
-    elementsNoEventDefinitionRequired: [
-
-      // 1.0
-      'bpmn:EndEvent',
-      'bpmn:StartEvent',
-
-      // 1.1
-      'bpmn:IntermediateThrowEvent'
-    ],
-    eventDefinitions: {
-
-      // 1.1
-      'bpmn:BoundaryEvent': [
-        'bpmn:ErrorEventDefinition',
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ],
-      'bpmn:EndEvent': [
-
-        // 1.0
-        'bpmn:ErrorEventDefinition',
-
-        // 1.1
-        'bpmn:MessageEventDefinition'
-      ],
-      'bpmn:IntermediateCatchEvent': [
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ],
-      'bpmn:StartEvent': [
-        'bpmn:ErrorEventDefinition',
-        'bpmn:MessageEventDefinition',
-        'bpmn:TimerEventDefinition'
-      ],
-
-      // 1.2
-      'bpmn:IntermediateThrowEvent': [
-        'bpmn:MessageEventDefinition'
-      ]
-    }
+    elements: camundaCloud12Elements,
+    elementsNoEventDefinitionRequired: camundaCloud12ElementsNoEventDefinitionRequired,
+    eventDefinitions: camundaCloud12EventDefinitions
   }
 };
