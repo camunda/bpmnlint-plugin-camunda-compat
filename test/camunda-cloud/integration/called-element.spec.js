f03f043ab3b9bdc7ae4e18d048179382545c0b3e
Philipp <philippfromme@outlook.com>
2022-10-31 16:20:58 +0100

    test: add `called-element` integration spec

---
+++ test/camunda-cloud/integration/called-element.spec.js
@@ -0,0 +1,73 @@
+const { expect } = require('chai');
+
+const Linter = require('bpmnlint/lib/linter');
+
+const NodeResolver = require('bpmnlint/lib/resolver/node-resolver');
+
+const { readModdle } = require('../../helper');
+
+const versions = [
+  '1.0',
+  '1.1',
+  '1.2',
+  '1.3',
+  '8.0',
+  '8.1'
+];
+
+describe('integration - called-element', function() {
+
+  versions.forEach(function(version) {
+
+    let linter;
+
+    beforeEach(function() {
+      linter = new Linter({
+        config: {
+          extends: `plugin:camunda-compat/camunda-cloud-${ version.replace('.', '-') }`
+        },
+        resolver: new NodeResolver()
+      });
+    });
+
+
+    describe(`Camunda Cloud ${ version }`, function() {
+
+      describe('no errors', function() {
+
+        it('should not have errors', async function() {
+
+          // given
+          const { root } = await readModdle('test/camunda-cloud/integration/called-element.bpmn');
+
+          // when
+          const reports = await linter.lint(root);
+
+          // then
+          expect(reports[ 'camunda-compat/called-element' ]).not.to.exist;
+        });
+
+      });
+
+
+      describe('errors', function() {
+
+        it('should have errors', async function() {
+
+          // given
+          const { root } = await readModdle('test/camunda-cloud/integration/called-element-errors.bpmn');
+
+          // when
+          const reports = await linter.lint(root);
+
+          // then
+          expect(reports[ 'camunda-compat/called-element' ]).to.exist;
+        });
+
+      });
+
+    });
+
+  });
+
+});
