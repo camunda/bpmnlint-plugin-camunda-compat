c5e174ee058109c248db60716bb8141809008b0e
Philipp <philippfromme@outlook.com>
2022-11-01 09:58:00 +0100

    chore: code style

--- rules/utils/error-types.js
+++ rules/utils/error-types.js
@@ -1,15 +1,15 @@
 module.exports.ERROR_TYPES = Object.freeze({
   ELEMENT_COLLAPSED_NOT_ALLOWED: 'camunda.elementCollapsedNotAllowed',
   ELEMENT_TYPE_NOT_ALLOWED: 'camunda.elementTypeNotAllowed',
+  EXPRESSION_REQUIRED: 'camunda.expressionRequired',
+  EXPRESSION_VALUE_NOT_ALLOWED: 'camunda.expressionValueNotAllowed',
   EXTENSION_ELEMENT_NOT_ALLOWED: 'camunda.extensionElementNotAllowed',
   EXTENSION_ELEMENT_REQUIRED: 'camunda.extensionElementRequired',
+  FEEL_INVALID: 'camunda.feelInvalid',
   PROPERTY_DEPENDEND_REQUIRED: 'camunda.propertyDependendRequired',
   PROPERTY_NOT_ALLOWED: 'camunda.propertyNotAllowed',
   PROPERTY_REQUIRED: 'camunda.propertyRequired',
   PROPERTY_TYPE_NOT_ALLOWED: 'camunda.propertyTypeNotAllowed',
   PROPERTY_VALUE_DUPLICATED: 'camunda.propertyValueDuplicated',
-  PROPERTY_VALUE_NOT_ALLOWED: 'camunda.propertyValueNotAllowed',
-  FEEL_INVALID: 'camunda.feelInvalid',
-  EXPRESSION_REQUIRED: 'camunda.expressionRequired',
-  EXPRESSION_VALUE_NOT_ALLOWED: 'camunda.expressionValueNotAllowed'
+  PROPERTY_VALUE_NOT_ALLOWED: 'camunda.propertyValueNotAllowed'
 });
