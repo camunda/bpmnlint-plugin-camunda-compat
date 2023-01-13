f8c7b3225f3ea691f5500783f98c48ce5cfd3f0b
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2023-01-13 10:48:08 +0100

    fix: disallow error code expression for catch events
    Closes #74

--- test/camunda-cloud/no-expression.spec.js
+++ test/camunda-cloud/no-expression.spec.js
@@ -104,6 +104,65 @@ const invalid = [
         allowedVersion: '8.2'
       }
     }
+  },
+  {
+    name: 'error start event (static expression-like error code)',
+    config: { version: '8.2' },
+    moddleElement: createModdle(createDefinitions(`
+      <bpmn:process id="Process_1">
+        <bpmn:subProcess id="EventSubProcess" triggeredByEvent="true">
+          <bpmn:startEvent id="Event">
+            <bpmn:errorEventDefinition id="ErrorEventDefinition_1" errorRef="Error"/>
+          </bpmn:startEvent>
+        </bpmn:subProcess>
+      </bpmn:process>
+      <bpmn:error id="Error" name="Error" errorCode="=myCode" />
+    `)),
+    report: {
+      id: 'Event',
+      message: 'Expression statement <=myCode> not supported',
+      path: [
+        'rootElements',
+        1,
+        'errorCode'
+      ],
+      data: {
+        type: ERROR_TYPES.EXPRESSION_NOT_ALLOWED,
+        node: 'Error',
+        parentNode: 'Event',
+        property: 'errorCode',
+        allowedVersion: null
+      }
+    }
+  },
+  {
+    name: 'error boundary event (static expression-like error code)',
+    config: { version: '8.2' },
+    moddleElement: createModdle(createDefinitions(`
+      <bpmn:process id="Process_1">
+        <bpmn:Task id="Task_1" />
+        <bpmn:boundaryEvent id="Event" attachedToRef="Task_1">
+          <bpmn:errorEventDefinition id="ErrorEventDefinition_1" errorRef="Error"/>
+        </bpmn:boundaryEvent>
+      </bpmn:process>
+      <bpmn:error id="Error" name="Error" errorCode="=myCode" />
+    `)),
+    report: {
+      id: 'Event',
+      message: 'Expression statement <=myCode> not supported',
+      path: [
+        'rootElements',
+        1,
+        'errorCode'
+      ],
+      data: {
+        type: ERROR_TYPES.EXPRESSION_NOT_ALLOWED,
+        node: 'Error',
+        parentNode: 'Event',
+        property: 'errorCode',
+        allowedVersion: null
+      }
+    }
   }
 ];
 
