f1bc1d71295d71556dda7e02e20945c84dffc8f4
Beatriz Mendes <bea.smendes98@gmail.com>
2023-03-09 20:49:24 +0100

    feat(platform): add `history-to-live` rule
    Related to https://github.com/camunda/camunda-modeler/issues/3369

---
+++ test/camunda-platform/history-time-to-live.spec.js
@@ -0,0 +1,54 @@
+const RuleTester = require('bpmnlint/lib/testers/rule-tester');
+
+const rule = require('../../rules/history-time-to-live');
+
+const {
+  createDefinitions,
+  createModdle
+} = require('../helper');
+
+const { ERROR_TYPES } = require('../../rules/utils/element');
+
+const valid = [
+  {
+    name: 'process (history time to live)',
+    moddleElement: createModdle(createDefinitions(`
+      <bpmn:process id="Process_1" isExecutable="true" camunda:historyTimeToLive="123"/>
+    `), 'platform'),
+  },
+  {
+    name: 'non-executable process (history time to live)',
+    config: { platform: 'platform' },
+    moddleElement: createModdle(createDefinitions(`
+      <bpmn:process id="Process_1" />
+    `), 'platform'),
+  }
+];
+
+const invalid = [
+  {
+    name: 'process (no history time to live)',
+    moddleElement: createModdle(createDefinitions(`
+    <bpmn:process id="Process_1" isExecutable="true">
+    </bpmn:process>
+    `), 'platform'),
+    report: {
+      id: 'Process_1',
+      message: 'Element of type <bpmn:Process> must have property <historyTimeToLive>',
+      path: [
+        'historyTimeToLive'
+      ],
+      data: {
+        type: ERROR_TYPES.PROPERTY_REQUIRED,
+        node: 'Process_1',
+        parentNode: null,
+        requiredProperty: 'historyTimeToLive'
+      }
+    }
+  }
+];
+
+RuleTester.verify('history-time-to-live', rule, {
+  valid,
+  invalid
+});
