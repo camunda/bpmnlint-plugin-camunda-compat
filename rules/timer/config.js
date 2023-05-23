ad916566796c93ea1dee6f12bd16798e57fdf2ee
Philipp <philippfromme@outlook.com>
2023-05-23 15:58:20 +0200

    feat(timer): add config per element type
    * configs for expression type and per element type exist and can be used to lint

--- rules/timer/config.js
+++ rules/timer/config.js
@@ -1,4 +1,47 @@
+const { is } = require('bpmnlint-utils');
+
 module.exports = {
-  'cron': '8.1',
-  'iso8601': '1.0'
+  expressionType: {
+    'cron': '8.1',
+    'iso8601': '1.0'
+  },
+  elementType: {
+    'bpmn:StartEvent': {
+      'timeCycle': (isInterrupting, parent) => {
+        if (!isInterrupting || !isEventSubProcess(parent)) {
+          return '1.0';
+        }
+
+        return null;
+      },
+      'timeDate': () => '1.0',
+      'timeDuration': (_, parent) => {
+        if (isEventSubProcess(parent)) {
+          return '1.0';
+        }
+
+        return null;
+      }
+    },
+    'bpmn:BoundaryEvent': {
+      'timeCycle': (cancelActivity) => {
+        if (!cancelActivity) {
+          return '1.0';
+        }
+
+        return null;
+      },
+      'timeDate': () => null,
+      'timeDuration': () => '1.0'
+    },
+    'bpmn:IntermediateCatchEvent': {
+      'timeCycle': () => null,
+      'timeDate': () => null,
+      'timeDuration': () => '1.0'
+    }
+  }
 };
+
+function isEventSubProcess(element) {
+  return is(element, 'bpmn:SubProcess') && element.get('triggeredByEvent') === true;
+}
