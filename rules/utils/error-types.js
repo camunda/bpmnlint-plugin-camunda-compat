ee61ede4eb705ddfcf198b5541a7aa088df07f85
Oleksii Ivanov <108869886+Oleksiivanov@users.noreply.github.com>
2023-08-15 15:55:37 +0300

    Revert "chore: refactor `secrets` rule"
    This reverts commit 53949e65919ffb3b6e653d6a88c2d5c537027a77.

--- rules/utils/error-types.js
+++ rules/utils/error-types.js
@@ -18,5 +18,5 @@ module.exports.ERROR_TYPES = Object.freeze({
   PROPERTY_VALUE_DUPLICATED: 'camunda.propertyValueDuplicated',
   PROPERTY_VALUE_NOT_ALLOWED: 'camunda.propertyValueNotAllowed',
   PROPERTY_VALUE_REQUIRED: 'camunda.propertyValueRequired',
-  SECRET_EXPRESSION_INVALID: 'camunda.secretExpressionInvalid'
+  INVALID_SECRET_FORMAT: 'camunda.invalidSecretFormat'
 });
