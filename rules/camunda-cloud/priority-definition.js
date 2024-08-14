c74c3d28c34ba20c98923834d1db618d62572858
Valentin Serra <serval.core@gmail.com>
2024-08-14 13:03:42 +0200

    feat: add `no-priority-definition` and `priority-definition` rules
    Closes #170
    
    deps: bump zeebe-bpmn-moddle@1.5.1

---
+++ rules/camunda-cloud/priority-definition.js
@@ -0,0 +1,62 @@
+const { is } = require('bpmnlint-utils');
+const { isDefined } = require('min-dash');
+
+const {
+  findExtensionElement,
+  hasExpression
+} = require('../utils/element');
+
+const { reportErrors } = require('../utils/reporter');
+
+const { skipInNonExecutableProcess } = require('../utils/rule');
+
+module.exports = skipInNonExecutableProcess(function() {
+  function check(node, reporter) {
+    if (!is(node, 'bpmn:UserTask')) {
+      return;
+    }
+
+    const priorityDefinition = findExtensionElement(node, 'zeebe:PriorityDefinition');
+
+    if (!priorityDefinition) {
+      return;
+    }
+
+    const priority = priorityDefinition.get('priority');
+
+    let errors = [];
+
+    if (isDefined(priority)) {
+      errors = [
+        ...errors,
+        ...hasExpression(priorityDefinition, 'priority', {
+          allowed: value => isValidPriority(value)
+        }, node)
+      ];
+    }
+
+    if (errors.length) {
+      reportErrors(node, reporter, errors);
+    }
+  }
+
+  return {
+    check
+  };
+});
+
+function isValidPriority(value) {
+  return isExpression(value) || isIntegerStringBetween(value, 0, 100);
+}
+
+function isExpression(value) {
+  return value.startsWith('=');
+}
+
+function isIntegerStringBetween(value, min, max) {
+  if (/^\d+$/.test(value)) {
+    const number = parseInt(value, 10);
+    return number >= min && number <= max;
+  }
+  return false;
+}
---
+++ test/camunda-cloud/integration/priority-definition.spec.js
@@ -0,0 +1,68 @@
+const { expect } = require('chai');
+
+const Linter = require('bpmnlint/lib/linter');
+
+const NodeResolver = require('bpmnlint/lib/resolver/node-resolver');
+
+const { readModdle } = require('../../helper');
+
+const versions = [
+  '8.6'
+];
+
+describe('integration - priority definition', function() {
+
+  versions.forEach(function(version) {
+
+    let linter;
+
+    beforeEach(function() {
+      linter = new Linter({
+        config: {
+          extends: `plugin:camunda-compat/camunda-cloud-${ version.replace('.', '-') }`
+        },
+        resolver: new NodeResolver()
+      });
+    });
+
+
+    describe(`Camunda Cloud ${ version }`, function() {
+
+      describe('no errors', function() {
+
+        it('should not have errors', async function() {
+
+          // given
+          const { root } = await readModdle('test/camunda-cloud/integration/priority-definition.bpmn');
+
+          // when
+          const reports = await linter.lint(root);
+
+          // then
+          expect(reports[ 'camunda-compat/priority-definition' ]).not.to.exist;
+        });
+
+      });
+
+
+      describe('errors', function() {
+
+        it('should have errors', async function() {
+
+          // given
+          const { root } = await readModdle('test/camunda-cloud/integration/priority-definition-errors.bpmn');
+
+          // when
+          const reports = await linter.lint(root);
+
+          // then
+          expect(reports[ 'camunda-compat/priority-definition' ]).to.exist;
+        });
+
+      });
+
+    });
+
+  });
+
+});
