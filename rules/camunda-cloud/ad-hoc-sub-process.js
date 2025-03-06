ff4e5e81c1c5fa332a5a1f2c3a58a464023c5132
Mathias Geat <mathias.geat@camunda.com>
2025-03-06 15:43:24 +0100

    feat: add rules for ad-hoc subprocess completionCondition & cancelRemainingInstances only supported from camunda 8.8

--- rules/camunda-cloud/ad-hoc-sub-process.js
+++ rules/camunda-cloud/ad-hoc-sub-process.js
@@ -3,25 +3,53 @@ const { is, isAny } = require('bpmnlint-utils');
 const { reportErrors } = require('../utils/reporter');
 
 const { skipInNonExecutableProcess } = require('../utils/rule');
+const { greaterOrEqual } = require('../utils/version');
 
-module.exports = skipInNonExecutableProcess(function() {
+const COMPLETION_ATTRIBUTES_SUPPORT_VERSION = '8.8';
+
+module.exports = skipInNonExecutableProcess(function({ version }) {
   function check(node, reporter) {
     if (!is(node, 'bpmn:AdHocSubProcess')) {
       return;
     }
 
     // Ad-Hoc Sub-Process must contain at least one activity
-    if (node.get('flowElements').some(isActivity)) {
-      return;
+    if (!node.get('flowElements').some(isActivity)) {
+      reportErrors(node, reporter, {
+        message: 'Element of type <bpmn:AdHocSubProcess> must contain at least one activity',
+        data: {
+          node,
+          parentNode: null
+        }
+      });
     }
 
-    reportErrors(node, reporter, {
-      message: 'Element of type <bpmn:AdHocSubProcess> must contain at least one activity',
-      data: {
-        node,
-        parentNode: null
+    if (!greaterOrEqual(version, COMPLETION_ATTRIBUTES_SUPPORT_VERSION)) {
+      if (node.get('completionCondition')) {
+        reportErrors(node, reporter, [
+          {
+            message:  'Element of type <bpmn:completionCondition> within <bpmn:AdHocSubProcess> only allowed by Camunda 8.8 or newer',
+            data: {
+              node,
+              parentNode: null
+            }
+          }
+        ]);
       }
-    });
+
+      // check for property existence here as the attributes defaults to true and a value will always be present
+      if (Object.prototype.hasOwnProperty.call(node, 'cancelRemainingInstances')) {
+        reportErrors(node, reporter, [
+          {
+            message:  'Element of type <bpmn:AdHocSubProcess> with property <cancelRemainingInstances> only allowed by Camunda 8.8 or newer',
+            data: {
+              node,
+              parentNode: null
+            }
+          }
+        ]);
+      }
+    }
   }
 
   return {
--- test/camunda-cloud/ad-hoc-sub-process.spec.js
+++ test/camunda-cloud/ad-hoc-sub-process.spec.js
@@ -10,17 +10,48 @@ const {
 const valid = [
   {
     name: 'ad hoc sub process (with task)',
+    config: { version: '8.7' },
     moddleElement: createModdle(createProcess(`
       <bpmn:adHocSubProcess id="Subprocess_1">
         <bpmn:task id="Task_1" />
       </bpmn:adHocSubProcess>
     `))
-  }
+  },
+  {
+    name: 'ad hoc sub process (with completionCondition)',
+    config: { version: '8.8' },
+    moddleElement: createModdle(createProcess(`
+      <bpmn:adHocSubProcess id="Subprocess_1">
+        <bpmn:task id="Task_1" />
+        <bpmn:completionCondition xsi:type="bpmn:tFormalExpression">=myCondition</bpmn:completionCondition>
+      </bpmn:adHocSubProcess>
+    `))
+  },
+  {
+    name: 'ad hoc sub process (with cancelRemainingInstances attribute)',
+    config: { version: '8.8' },
+    moddleElement: createModdle(createProcess(`
+      <bpmn:adHocSubProcess id="Subprocess_1" cancelRemainingInstances="false">
+        <bpmn:task id="Task_1" />
+      </bpmn:adHocSubProcess>
+    `))
+  },
+  {
+    name: 'ad hoc sub process (with completionCondition and cancelRemainingInstances attribute)',
+    config: { version: '8.8' },
+    moddleElement: createModdle(createProcess(`
+      <bpmn:adHocSubProcess id="Subprocess_1" cancelRemainingInstances="false">
+        <bpmn:task id="Task_1" />
+        <bpmn:completionCondition xsi:type="bpmn:tFormalExpression">=myCondition</bpmn:completionCondition>
+      </bpmn:adHocSubProcess>
+    `))
+  },
 ];
 
 const invalid = [
   {
     name: 'ad hoc sub process (empty)',
+    config: { version: '8.7' },
     moddleElement: createModdle(createProcess(`
       <bpmn:adHocSubProcess id="Subprocess_1">
       </bpmn:adHocSubProcess>
@@ -36,6 +67,7 @@ const invalid = [
   },
   {
     name: 'ad hoc sub process (no activity)',
+    config: { version: '8.7' },
     moddleElement: createModdle(createProcess(`
       <bpmn:adHocSubProcess id="Subprocess_1">
         <bpmn:exclusiveGateway id="Gateway_1" />
@@ -50,10 +82,45 @@ const invalid = [
         parentNode: null
       }
     }
-  }
+  },
+  {
+    name: 'ad hoc sub process (with completionCondition)',
+    config: { version: '8.7' },
+    moddleElement: createModdle(createProcess(`
+      <bpmn:adHocSubProcess id="Subprocess_1">
+        <bpmn:task id="Task_1" />
+        <bpmn:completionCondition xsi:type="bpmn:tFormalExpression">=myCondition</bpmn:completionCondition>
+      </bpmn:adHocSubProcess>
+    `)),
+    report: {
+      id: 'Subprocess_1',
+      message: 'Element of type <bpmn:completionCondition> within <bpmn:AdHocSubProcess> only allowed by Camunda 8.8 or newer',
+      data: {
+        node: 'Subprocess_1',
+        parentNode: null
+      }
+    }
+  },
+  {
+    name: 'ad hoc sub process (with cancelRemainingInstances attribute)',
+    config: { version: '8.7' },
+    moddleElement: createModdle(createProcess(`
+      <bpmn:adHocSubProcess id="Subprocess_1" cancelRemainingInstances="false">
+        <bpmn:task id="Task_1" />
+      </bpmn:adHocSubProcess>
+    `)),
+    report: {
+      id: 'Subprocess_1',
+      message: 'Element of type <bpmn:AdHocSubProcess> with property <cancelRemainingInstances> only allowed by Camunda 8.8 or newer',
+      data: {
+        node: 'Subprocess_1',
+        parentNode: null
+      }
+    }
+  },
 ];
 
-RuleTester.verify('called-element', rule, {
+RuleTester.verify('ad-hoc-sub-process', rule, {
   valid,
   invalid
 });
